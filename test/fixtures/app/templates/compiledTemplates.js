993cb2899c5556497e8e5d4f5483792f725f2901
Steffen Leistner <sleistner@gmail.com>
2014-08-05 17:12:24 +0200

    Initial commit.

---
+++ test/fixtures/app/templates/compiledTemplates.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function () {
+    return {
+        'my_test_template': function () {},
+        'subdir/other_template': function () {}
+    };
+};
---
+++ test/fixtures/app/templates/otherTemplates.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function () {
+    return {
+        'other_template_pattern/home_view': function () {},
+        'other_template_pattern/subdir/nav_view': function () {}
+    };
+};
